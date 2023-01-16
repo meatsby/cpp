16035a77b3dfa00cc4cce9737f35b1f5d33f3570
meatsby <meatsby@gmail.com>
2023-01-16 10:51:46 +0800

    feat: ex2.cpp

---
+++ Lab2/ex2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "In C++, \"\\\" indicates an escape sequence.\n\nFor example, \"\\n\" will output a new line, and \"\\\\\"\nwill output the backslash character \"\\\" itself." << endl;
+    return 0;
+}
