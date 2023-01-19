a1113b42091fc1bc8439114b29829140782a0441
meatsby <meatsby@gmail.com>
2023-01-19 20:34:01 +0800

    refactor: loop.cpp

--- Lab2/loop.cpp
+++ Lab2/loop.cpp
@@ -9,5 +9,44 @@ int main() {
     while (i <= 10) {
         printf("Hello world %d\n", i++);
     }
+
+    i = 1;
+    do {
+        printf("Hello world %d\n", i++);
+    } while (i <= 10);
+
+    for (int j = 2; j < 10; j++) {
+        for (int k = 2; k < 10; k++) {
+            printf("%d x %d = %d\n", j, k, j * k);
+        }
+    }
+
+    for (int j = 0; j < 5; j++) {
+        for (int k = 0; k < j; k++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    for (int j = 0; j < 5; j++) {
+        for (int k = 0; k < 5 - j; k++) {
+            printf(" ");
+        }
+        for (int k = 0; k < j; k++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    int floor = 5;
+    for (int i = 0; i < floor; i++) {
+        for (int j = 0; j < floor - i; j++) {
+            printf(" ");
+        }
+        for (int j = 0; j < (i * 2) + 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
     return 0;
 }
