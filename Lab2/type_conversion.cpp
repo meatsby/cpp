2d925ea4c474011a3bf3afbf83a837e44ab49887
meatsby <meatsby@gmail.com>
2023-01-20 16:23:58 +0800

    feat: type_conversion.cpp

---
+++ Lab2/type_conversion.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    double d = 1.23;
+    int b = d;
+    cout << b << "\n";
+
+    int i = 5;
+    char a = 'a';
+    double x = 1.57;
+    cout << i + x << endl;
+    cout << x * a << endl;
+    return 0;
+}
