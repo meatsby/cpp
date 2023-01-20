b518b87f76c14ff6ff1c3c501e95a69a182979b0
meatsby <meatsby@gmail.com>
2023-01-20 16:06:05 +0800

    feat: sizeof.cpp

---
+++ Lab2/sizeof.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int a = 4;
+    cout << sizeof(a) << endl;
+    cout << sizeof(int) << endl;
+    cout << sizeof(double) << endl;
+    cout << sizeof(long double) << endl;
+    return 0;
+}
