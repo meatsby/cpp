40f3b1e7c4bfb0290258cc1ff4025512d8b3df4c
meatsby <meatsby@gmail.com>
2023-01-20 17:01:20 +0800

    feat: scope.cpp

--- Lab2/operators.cpp
+++ Lab2/operators.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
-
 using namespace std;
 int main() {
-    int k = 1;
-    int a = k++; // a = 1, k = 2
-    int b = ++k; // b = 3, k = 3;
-    cout << k << endl; // 3
-    cout << a << endl; // 1
-    cout << b << endl; // 3
+    int x = 3;
+    cout << ++x; // 4
+    cout << x++; // 4
+    cout << x;   // 5
     return 0;
 }
