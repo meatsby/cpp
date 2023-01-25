a1bd5fe23ad201a4b45929cddc7f9a9e9f4f33de
meatsby <meatsby@gmail.com>
2023-01-25 19:39:23 +0800

    feat: array

---
+++ Study/array.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int subway_array[3];
+    subway_array[0] = 30;
+    subway_array[1] = 40;
+    subway_array[2] = 50;
+
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    char str[] = "coding";
+    printf("%s\n", str);
+    printf("%lu\n", sizeof(str));
+    return 0;
+}
