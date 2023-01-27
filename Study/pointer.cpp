362010e32eb4068e1ce36ee93bbb57c3657438b6
meatsby <meatsby@gmail.com>
2023-01-27 23:29:01 +0800

    feat: pointer

--- Study/pointer.cpp
+++ Study/pointer.cpp
@@ -16,5 +16,19 @@ int main() {
     *pointer_var *= 3;
     printf("address : %d, value : %d\n", pointer_var, *pointer_var);
     printf("address : %d, value : %d\n", &pointer_var, pointer_var);
+
+    int arr[3] = {5, 10, 15};
+    int * ptr = arr;
+    for (int i = 0; i < 3; i++) {
+        printf("배열 값 : %d\n", arr[i]);
+        printf("포인터 값 : %d\n", ptr[i]);
+    }
+    ptr[0] = 100;
+    ptr[1] = 200;
+    ptr[2] = 300;
+    for (int i = 0; i < 3; i++) {
+        printf("배열 값 : %d\n", arr[i]);
+        printf("포인터 값 : %d\n", ptr[i]);
+    }
     return 0;
 }
