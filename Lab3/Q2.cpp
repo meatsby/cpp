cca6f5a054d584d5a5a1d0a52b29999dee6b6b21
meatsby <meatsby@gmail.com>
2023-01-30 10:41:05 +0800

    feat: Q2.cpp

--- Lab3/Q2.cpp
+++ Lab3/Q2.cpp
@@ -8,8 +8,8 @@ int main() {
     int i = 5;
     char a = 'b';
     double x = 1.57;
-    i = i + x; // i + x will result 6.57 due to binary expression but will be denoted to int type because of assignment
-    x = x * a; // 1.57 * 'b' -> 1.57 * 98 (ASCII value for 'b' is 98)
+    i = i + x; // i + x will result 6.57 due to binary expression but will be demoted to int type because of assignment
+    x = x * a; // char a will be promoted to double. 1.57 * 'b' -> 1.57 * 98 (ASCII value for 'b' is 98)
 
     cout << i << "\n";
     cout << x << "\n";
