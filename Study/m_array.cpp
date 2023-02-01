3369a5270dc37f461b6a602b5f864fe7d063cd9d
meatsby <meatsby@gmail.com>
2023-02-01 18:35:07 +0800

    feat: m_array.cpp

--- Study/m_array.cpp
+++ Study/m_array.cpp
@@ -1,6 +1,15 @@
-#include <stdio.h>
+#include <iostream>
+
+using namespace std;
 
 int main() {
-    int arr2[2][5];
+    int arr2[2][5] = {{1, 2, 3, 4, 5},
+                      {1, 2, 3, 4, 5}};
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 5; j++) {
+            cout << arr2[i][j] << endl;
+        }
+    }
     return 0;
 }
