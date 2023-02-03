883f0c6def0be1b950cd453961c9343f68c25500
meatsby <meatsby@gmail.com>
2023-02-03 15:20:44 +0800

    feat: lec3

---
+++ Lecture3/review.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    int a = 1, b = 0;
+    b = 1.8 + (a++);
+    cout << b << endl;
+    cout << a << endl;
+
+    b += ++a;
+    cout << setprecision(1);
+    cout << scientific << (double )b << endl;
+    cout << a << endl;
+}
