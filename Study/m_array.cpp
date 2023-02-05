b1bbd1537ebb7d830da8cd10616c99d9cb445bfc
meatsby <meatsby@gmail.com>
2023-02-05 22:46:28 +0800

    feat: m_array

--- Study/m_array.cpp
+++ Study/m_array.cpp
@@ -11,5 +11,19 @@ int main() {
             cout << arr2[i][j] << endl;
         }
     }
+
+
+    int arr3[2][2][2] = {{{1, 2},
+                                 {1, 2}},
+                         {{1, 2},
+                                 {1, 2}}};
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            for (int k = 0; k < 2; k++) {
+                cout << arr3[i][j][k] << endl;
+            }
+        }
+    }
     return 0;
 }
