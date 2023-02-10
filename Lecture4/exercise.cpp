31c46e3155652b1f3e9d9ffee5e5eb9d3ca6e1a4
meatsby <meatsby@gmail.com>
2023-02-10 17:31:44 +0800

    feat: exercise

--- Lecture4/exercise.cpp
+++ Lecture4/exercise.cpp
@@ -3,15 +3,30 @@
 using namespace std;
 
 int main() {
-    int n, m;
-    cout << "Enter inputs: " << endl;
-    cin >> n >> m;
-    cout << n + m << endl;
-    cout << n * m << endl;
+//    int n, m;
+//    cout << "Enter inputs: " << endl;
+//    cin >> n >> m;
+//    cout << n + m << endl;
+//    cout << n * m << endl;
+//
+//    char c1 = 'A';
+//    int c2 = '0';
+//    cout << c1 << " " << c2 << endl;
+//    cout << ++c1 << endl;
 
-    char c1 = 'A';
-    int c2 = '0';
-    cout << c1 << " " << c2 << endl;
-    cout << ++c1 << endl;
+    int m = 3, n = 2;
+    double k;
+    k = m / n;
+    cout << k << endl;
+    k = m / double(n);
+    cout << k << endl;
+    k = double(m) / n;
+    cout << k << endl;
+    k = double(m / n);
+    cout << k << endl;
+    k = m / 2;
+    cout << k << endl;
+    k = m / 2.0;
+    cout << k << endl;
     return 0;
 }
