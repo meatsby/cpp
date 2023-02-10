f10c64a206b1913623046a5e4f366edc4d8bf11a
meatsby <meatsby@gmail.com>
2023-02-10 17:20:25 +0800

    feat: exercise

---
+++ Lecture4/exercise.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cout << "Enter inputs: " << endl;
+    cin >> n >> m;
+    cout << n + m << endl;
+    cout << n * m << endl;
+
+    char c1 = 'A';
+    int c2 = '0';
+    cout << c1 << " " << c2 << endl;
+    cout << ++c1 << endl;
+    return 0;
+}
