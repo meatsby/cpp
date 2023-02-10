fd459d88feca2b0bb68947fabf88336366e7dd20
meatsby <meatsby@gmail.com>
2023-02-10 16:25:58 +0800

    feat: for_error

---
+++ Lecture4/for.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int sum = 0, j;
+    for (j = 1; j <= 10; j++);
+    sum += j;
+    cout << sum << endl;
+    cout << j << endl;
+    return 0;
+}
