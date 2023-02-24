86df9da2f605b12296e52a069b892fb3e708e04e
meatsby <meatsby@gmail.com>
2023-02-24 16:11:46 +0800

    feat: read and writing array

---
+++ CS2311/Lecture6/read_and_write_array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#define N 10
+
+using namespace std;
+
+int main() {
+    int marks[N], sum = 0;
+    for (int i = 0; i < N; i++) {
+        cin >> marks[i];
+    }
+    cout << "The mark of the students are: ";
+    for (int i = 0; i < N; i++) {
+        cout << marks[i];
+        sum += marks[i];
+    }
+    cout << "Average mark = " << (float) sum / N << endl;
+}
