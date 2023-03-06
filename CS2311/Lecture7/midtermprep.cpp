87e63e4e1febb7c28da6eeb8345af87e5647ec26
meatsby <meatsby@gmail.com>
2023-03-06 16:26:22 +0800

    feat: midtermprep 2

--- CS2311/Lecture7/midtermprep.cpp
+++ CS2311/Lecture7/midtermprep.cpp
@@ -3,12 +3,25 @@
 using namespace std;
 
 int main() {
+    // 1.1.
     int sum = 0;
     cout << sum++ << endl;
+
+    // 1.2.
     double a = 0.5;
     bool b = 0<a && a<1;
     cout << b << endl;
     bool c = 0<a<1;
     cout << c << endl;
+
+    // 2.
+    int i = 1;
+    while (i <= 15) {
+        i++;
+        if (i%3 != 2) continue;
+        else cout << "i=" << i << endl;
+    }
+    // i=2, 5, 8, 11, 14
+
     return 0;
 }
