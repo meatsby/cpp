555b5d7fa1e7578d94a570759ee6d73285118220
meatsby <meatsby@gmail.com>
2023-03-10 13:36:08 +0800

    feat: midtermprep ch2

--- CS2311/Lecture7/review.cpp
+++ CS2311/Lecture7/review.cpp
@@ -3,9 +3,26 @@
 using namespace std;
 
 int main() {
-    int m = 3, n = 2;
-    double k;
-    k = double(m/n);
-    cout << k << endl;
+    // Ch2. Data, Operators, Basic I/O
+    // - Identifiers only can start with letter or int
+    int CityU_CS;
+    // - Overflow and underflow
+    int n = 2147483647;
+    cout << n + 1 << endl;
+    // - Type Conversion
+    cout << 3.14 * 2 * 2 << endl; // Implicit
+    cout << (int) 3.14 << endl;   // Explicit (Type-casting)
+    // - Pre & Post Operators
+    int x = 3;
+    cout << ++x << endl;
+    cout << x++ << endl;
+    cout << x << endl;
+    // - Bitwise Operators ('&', '|', '^', '~')
+    int c = 3 & 5;
+    cout << c << endl;
+    // - cout.width()
+    cout.width(5); // cout << setw(5);
+    cout << 1234567 << endl;
+    // - cout << setprecision(2) << fixed << scientific;
     return 0;
 }
