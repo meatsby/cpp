250cf824acb26f77d0601fa1236b1e0bc4f4aa1b
meatsby <meatsby@gmail.com>
2023-03-21 21:47:39 +0800

    feat: PartB

--- CS2311/Assignment2/PartB.cpp
+++ CS2311/Assignment2/PartB.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <iomanip>
 #include <cstring>
 
 using namespace std;
@@ -9,27 +8,65 @@ int main() {
     cout << "Enter Header:" << endl;
     cin.getline(str, 257);
 
-    int keys[257][7];
+    char keys[257][7];
 
-    // Handle Headers with length ~ 247
     int comb[7] = {1, 3, 7, 15, 31, 63, 127};
     int idx = 0, len = strlen(str);
     for (int i = 0; i < 7; i++) {
         for (int j = 1; j <= comb[i] && idx < len; j++, idx++) {
+            // generate binary
             int temp = j;
             int binary = 0;
             for (int k = 1; temp > 0; k *= 10) {
                 binary += (temp % 2) * k;
                 temp /= 2;
             }
-            for (int k = 6; k >= 0; k--) {
-                keys[idx][k] = binary % 10;
+
+            // store string
+            for (int k = i; k >= 0; k--) {
+                keys[idx][k] = char(binary % 10 + 48);
                 binary /= 10;
             }
-            for (int k = 0; k < 7; k++) {
-                cout << keys[idx][k];
+            keys[idx][i + 1] = '\0';
+        }
+    }
+
+    char code[257];
+    cout << "Enter code:";
+    cin.getline(code, 257);
+
+    bool first = true;
+    int length, i = 0;
+    while (true) {
+        if (first) {
+            length = (int(code[i]) - 48) * 4 + (int(code[i + 1]) - 48) * 2 + (int(code[i + 2]) - 48);
+            i += 3;
+            first = false;
+        }
+
+        char key[7];
+        bool isEnd = true;
+        for (int k = 0; k < length; i++, k++) {
+            if (code[i] == '1') {
+                isEnd = false;
             }
-            cout << endl;
+            key[k] = code[i];
+        }
+        key[length] = '\0';
+
+        if (isEnd) {
+            if (code[i + 1] == '\0') {
+                break;
+            }
+            first = true;
+            continue;
+        }
+
+        for (int ki = 0; ki < 257; ki++) {
+            if (strcmp(key, keys[ki]) == 0) {
+                cout << str[ki];
+                break;
+            }
         }
     }
 
