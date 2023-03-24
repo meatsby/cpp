639a7bf45bc511ff2f45aab53114876001ed00b7
meatsby <meatsby@gmail.com>
2023-03-24 17:35:43 +0800

    feat: pointer of pointer

--- CS2311/Lecture10/pointerOfPointer.cpp
+++ CS2311/Lecture10/pointerOfPointer.cpp
@@ -8,18 +8,25 @@ void skipSpaces(char *p) {
     cout << "Address: " << &p << " | Result: " << p << endl;
 }
 
-void skipSpaces(char **p) {
+void pointerOfPointer(char **p) {
     while (**p == ' ')
         (*p)++;
     cout << "Address: " << &(*p) << " | Result: " << *p << endl;
 }
 
+void pointerReference(char *&p) {
+    while (*p == ' ')
+        p++;
+    cout << "Address: " << &p << " | Result: " << p << endl;
+}
+
 int main() {
     char str[] = "    hello";
     char *p = str;
-    skipSpaces(p);     // Address: 0x16ce23288 | Result: hello
-    cout << p << endl; // Address: 0x16ce232c8 | Result:     hello
-    skipSpaces(&p);    // Address: 0x16ce232c8 | Result: hello
-    cout << p << endl; // Address: 0x16ce232c8 | Result: hello
+    skipSpaces(p);           // Address: 0x16ce23288 | Result: hello
+    cout << p << endl;       // Address: 0x16ce232c8 | Result:     hello
+    pointerOfPointer(&p);    // Address: 0x16ce232c8 | Result: hello
+    pointerReference(p);  // Address: 0x16ce232c8 | Result: hello
+    cout << p << endl;       // Address: 0x16ce232c8 | Result: hello
     return 0;
 }
