dd844e260614a9cc702e0b1368b8577fca0278e3
meatsby <meatsby@gmail.com>
2023-03-27 10:37:52 +0800

    feat: Q2

---
+++ CS2311/Lab11/Q2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int countWord(char *arr, int *count) {
+    char *rPtr = arr - 1;
+    for (; *arr != '\0'; arr++) {
+        if (*(arr + 1) == ' ' || *(arr + 1) == '\0') {
+            (*count)++;
+            cout << "Word " << *count << " has " << arr - rPtr << " characters." << endl;
+            rPtr = arr + 1;
+        }
+    }
+    cout << "The number of words in the string is: " << *count << endl;
+}
+
+int main() {
+    cout << "Enter the content of the string:" << endl;
+
+    char str[100];
+    int count = 0;
+    cin.getline(str, 100);
+
+    countWord(str, &count);
+}
