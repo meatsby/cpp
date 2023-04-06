901a5f085c93b3f9a86b97b9c19b7fd02efb3c72
meatsby <meatsby@gmail.com>
2023-04-06 14:19:26 +0800

    feat: lab12 Q3

--- CS2311/Lab12/Point3D.cpp
+++ CS2311/Lab12/Point3D.cpp
@@ -1,74 +1,66 @@
+#include <iostream>
+#include <iomanip>
 #include <cmath>
-// add preprocessor here to make the program work
 
+using namespace std;
 
-
-class Point3D{
+class Point3D {
 private:
     double x;
     double y;
     double z;
 public:
-    /*
-    Replace the two constructors with one default constructor in the form of initializer list
-    */
+    Point3D(double x, double y, double z) : x(x), y(y), z(z) {}
 
-
-    Point3D();
-    // Overloaded constructor with three parameters
-    Point3D(double x, double y, double z);
     //set (mutator) functions
     void setX(double x);
+
     void setY(double y);
+
     void setZ(double z);
-    //get (accessor) functions       
+
+    //get (accessor) functions
     double getX();
+
     double getY();
+
     double getZ();
+
     //overridden function, returns the distance from point (0,0,0)
     double calDistance();
+
     //friend function to overload the operator << for cout
     //complete the function declaration with two parameters (ostream& output, Point3D& point)
-    
-
+    friend void print(ostream &output, Point3D &point);
 };
 
-Point3D::Point3D(){
-    x = 0.0;
-    y = 0.0;
-    z = 0.0;
+void Point3D::setX(double x) {
+    this->x = x;
 }
 
-Point3D::Point3D(double x, double y, double z){
-    this->x = x;
+void Point3D::setY(double y) {
     this->y = y;
+}
+
+void Point3D::setZ(double z) {
     this->z = z;
 }
 
-void Point3D::setX(double x){
-    this->x = x;
-}
-void Point3D::setY(double y){
-    this->y = y;
-}
-void Point3D::setZ(double z){
-    this->z = z;
-}
-
-double Point3D::getX(){
+double Point3D::getX() {
     return x;
 }
-double Point3D::getY(){
+
+double Point3D::getY() {
     return y;
 }
-double Point3D::getZ(){
+
+double Point3D::getZ() {
     return z;
 }
 
 // complete this function
 double Point3D::calDistance() {
-    
-
+    return sqrt(x * x + y * y + z * z);
 }
 
 // complete the function definition with two parameters (ostream & output, Point3D & point)
@@ -76,19 +68,18 @@ double Point3D::calDistance() {
 This point is: (..., ..., ...)
 The distance from (0, 0, 0) is ...
 */
-
-
-
-
+void print(ostream &output, Point3D &point) {
+    output << fixed << setprecision(2);
+    output << "This point is: (" << point.x << ", " << point.y << ", " << point.z << ")" << endl;
+    output << "The distance from (0, 0, 0) is " << point.calDistance() << endl;
+}
 
 
 int main() {
-    cout << "Enter a 3D point's x, y, and z coordinatese in order: " << endl;
-    // add code here
-    
-
-
-
-
+    double x, y, z;
+    cout << "Enter a 3D point's x, y, and z coordinates in order:" << endl;
+    cin >> x >> y >> z;
+    Point3D *point3D = new Point3D(x, y, z);
+    print(cout, *point3D);
     return 0;
 }
