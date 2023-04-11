97b2782987a7f79fd6081fa144ea3bdb8aa5b38a
meatsby <meatsby@gmail.com>
2023-04-11 21:48:04 +0800

    feat: uno game

--- CS2311/Assignment3/asg3_skeleton_partB.cpp
+++ CS2311/Assignment3/asg3_skeleton_partB.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-#include<cstring>
 
 using namespace std;
 
@@ -8,83 +7,125 @@ private:
     char *color;
     int value;
     int number;
+
 public:
-    Card();
+    Card() {
+        color = new char[10];
+        value = 0;
+        number = 0;
+    }
 
-    void setColor(char *n);
+    Card(char *color, int value, int number) : color(color), value(value), number(number) {};
 
-    void setValue(int v);
+    char *getColor() {
+        return color;
+    }
 
-    void setNum(int num);
+    int getVal() {
+        return value;
+    }
 
-    char *getColor();
+    int getNum() {
+        return number;
+    }
 
-    int getVal();
+    void setValue(int v) {
+        value = v;
+    }
 
-    int getNum();
+    bool compatible(Card &card) {
+        return (strcmp(color, card.color) == 0 || number == card.number) && value != 0;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, Card &card) {
+        out << card.color << card.number;
+        return out;
+    }
+
+    friend bool operator==(Card &c1, Card &c2) {
+        return c1.value == c2.value && c1.number == c2.number;
+    }
 };
 
-// Add your code here.
-// You need to implement these access functions and the default constructor in order to complete the class definition of Card.
-
-Card::Card() {
-    color = new char[10];
-    value = 0;
-    number = 0;
-}
-
-void Card::setColor(char *n) {
-    color = n;
-}
-
-void Card::setValue(int v) {
-    value = v;
-}
-
-void Card::setNum(int num) {
-    number = num;
-}
-
-char *Card::getColor() {
-    return color;
-}
-
-int Card::getVal() {
-    return value;
-}
-
-int Card::getNum() {
-    return number;
-}
-
-void initSequence(Card *cardSeq, char colorName[][10], int *color, int *num);
+void initSequence(Card *cardSeq, char colorName[][10], const int *num);
 
 void printSequence(Card *cardSeq);
+
+Card chooseCard(Card *cardSeq, Card previousCard) {
+    for (int i = 0; i < 6; i++) {
+        if (cardSeq[0].getVal() != 0) {
+            cardSeq[0].setValue(0);
+            return cardSeq[0];
+        }
+        if (cardSeq[i].getVal() == 0) {
+            continue;
+        }
+        if (cardSeq[i].compatible(previousCard)) {
+            cardSeq[i].setValue(0);
+            return cardSeq[i];
+        }
+    }
+    return *new Card();
+}
 
 // Add your code here.
 int main() {
     char colorName[4][10] = {"Red", "Yellow", "Blue", "Green"};
-    int color[4] = {1, 2, 3, 4};
-    int num[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-    Card cardSeq[6];
+    int input;
+    cout << "Enter the seed for random number generation: ";
+    cin >> input;
 
     // Add your code here.
-    initSequence(cardSeq, colorName, color, num);
+    int *num = new int(input);
+    Card cardSeq[6];
+    initSequence(cardSeq, colorName, num);
     cout << "Your card sequence: ";
     printSequence(cardSeq);
 
     // Add your code here for computer operation.
-    initSequence(ComcardSeq, colorName, colorc, numc);
+    int *numc = new int(8);
+    Card ComcardSeq[6];
+    initSequence(ComcardSeq, colorName, numc);
     cout << "Computer's card sequence: ";
     printSequence(ComcardSeq);
+
     // Add your code here.
+    Card previousCard = ComcardSeq[0];
+    while (true) {
+        Card c = chooseCard(ComcardSeq, previousCard);
+        if (c == *new Card()) {
+            cout << "Game ends. You win" << endl;
+            return 0;
+        }
+
+        cout << "Game continues. Your remaining card(s): ";
+        printSequence(cardSeq);
+
+        int colIn, numIn;
+        cout << "The computer plays " << c << ", please input two integers to represent the card you plan to play: ";
+        cin >> colIn >> numIn;
+
+        if (colIn == 0 && numIn == 0) {
+            cout << "Game ends. you lose" << endl;
+            return 0;
+        }
+
+        Card card = *new Card(colorName[colIn - 1], colIn, numIn);
+        for (int i = 0; i < 6; i++) {
+            if (card == cardSeq[i]) {
+                cardSeq[i].setValue(0);
+                previousCard = cardSeq[i];
+                break;
+            }
+        }
+    }
     return 0;
 }
 
 // Add your code here.
 // You need to implement initSequence(Card* cardSeq, char colorName[][10], int* color, int* num) function.
-void initSequence(Card *cardSeq, char colorName[][10], int *color, int *num) {
+void initSequence(Card *cardSeq, char colorName[][10], const int *num) {
     int cards[][6] = {{29, 35, 22, 31, 33, 23},
                       {23, 33, 14, 38, 22, 22},
                       {16, 32, 47, 35, 21, 21},
@@ -106,15 +147,11 @@ void initSequence(Card *cardSeq, char colorName[][10], int *color, int *num) {
                       {15, 27, 18, 34, 38, 43},
                       {47, 24, 31, 32, 37, 42}};
 
-    int seed;
-    cout << "Enter the seed for random number generation: ";
-    cin >> seed;
-
     for (int i = 0; i < 6; i++) {
-        Card *card = new Card();
-        card->setColor(colorName[cards[seed - 1][i] / 10 - 1]);
-        card->setNum(cards[seed - 1][i] % 10);
-        cardSeq[i] = *card;
+        char *c = colorName[cards[*num - 1][i] / 10 - 1];
+        int v = cards[*num - 1][i] / 10;
+        int n = cards[*num - 1][i] % 10;
+        cardSeq[i] = *new Card(c, v, n);
     }
 }
 
@@ -122,7 +159,10 @@ void initSequence(Card *cardSeq, char colorName[][10], int *color, int *num) {
 // You need to implement printSequence(Card* cardSeq) function.
 void printSequence(Card *cardSeq) {
     for (int i = 0; i < 6; i++) {
-        cout << cardSeq->getColor() << cardSeq->getNum() << " ";
-        cardSeq++;
+        Card card = cardSeq[i];
+        if (card.getVal() != 0) {
+            cout << card << " ";
+        }
     }
+    cout << endl;
 }
