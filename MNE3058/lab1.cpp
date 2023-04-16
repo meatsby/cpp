0fc9e3d6bc838f0d048e60b8e90f99d074a0b672
meatsby <meatsby@gmail.com>
2023-04-16 17:11:05 +0800

    feat: lab1.1

--- MNE3058/lab1.cpp
+++ MNE3058/lab1.cpp
@@ -1,17 +1,11 @@
-#define RED_LED 10
-#define YELLOW_LED 9
-#define GREEN_LED 8
-#define BUTTON_PIN 3
-
-volatile byte ledState = HIGH;
+#define RED_LED 11
+#define YELLOW_LED 10
+#define GREEN_LED 9
 
 void setup() {
     pinMode(RED_LED, OUTPUT);
     pinMode(YELLOW_LED, OUTPUT);
     pinMode(GREEN_LED, OUTPUT);
-    pinMode(BUTTON_PIN, INPUT);
-
-    attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), blinkLed, CHANGE);
 }
 
 void loop() {
@@ -19,19 +13,16 @@ void loop() {
     delay(1000);
     digitalWrite(GREEN_LED, LOW);
     delay(500);
-    for(int i=0;i<3;i++){
-        delay(500);
+
+    for(int i = 0; i < 3; i++) {
         digitalWrite(YELLOW_LED, ledState);
         delay(500);
         digitalWrite(YELLOW_LED, LOW);
+        delay(500);
     }
-    delay(500);
+
     digitalWrite(RED_LED, ledState);
     delay(1000);
     digitalWrite(RED_LED, LOW);
     delay(500);
-}
-
-void blinkLed() {
-    ledState = !ledState;
 }
