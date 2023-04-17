686405f0b268f03b23b31f77ce3e97e9c6cd51b0
meatsby <meatsby@gmail.com>
2023-04-17 19:23:26 +0800

    feat: lab2.3

--- MNE3058/lab2_3.cpp
+++ MNE3058/lab2_3.cpp
@@ -1,7 +1,8 @@
 #define RED_LED 13
 #define L_MOTOR 9
 #define R_MOTOR 5
-#define INTERRUPT 3
+#define STOP 3
+#define RESTART 2
 #define POT_PIN A0
 #define RPM_THRESHOLD 2000
 
@@ -12,8 +13,10 @@ void setup() {
     pinMode(RED_LED, OUTPUT);
     pinMode(L_MOTOR, OUTPUT);
     pinMode(R_MOTOR, OUTPUT);
-    pinMode(INTERRUPT, INPUT);
-    attachInterrupt(digitalPinToInterrupt(INTERRUPT), isr, CHANGE);
+    pinMode(STOP, INPUT);
+    pinMode(RESTART, INPUT);
+    attachInterrupt(digitalPinToInterrupt(STOP), stop, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(RESTART), restart, RISING);
 }
 
 void loop() {
@@ -23,8 +26,6 @@ void loop() {
         digitalWrite(RED_LED, HIGH);
         analogWrite(R_MOTOR, LOW);
         analogWrite(L_MOTOR, LOW);
-        delay(1000);
-        digitalWrite(RED_LED, LOW);
     } else {
         digitalWrite(RED_LED, LOW);
         analogWrite(R_MOTOR, val);
@@ -37,6 +38,10 @@ int read() {
     return map(potValue, 0, 1023, 0, 255);
 }
 
-void isr() {
+void stop() {
     safe = !safe;
+}
+
+void restart() {
+    digitalWrite(RED_LED, LOW);
 }
