4a9cb09a0f8bc02bf8f0269e7c7e5277423f2246
meatsby <meatsby@gmail.com>
2023-04-28 14:58:09 +0800

    feat: exam prep

--- CS2311/exam_prep.cpp
+++ CS2311/exam_prep.cpp
@@ -11,3 +11,31 @@ int main() {
     cout << temp[1] << endl;
     cout << &ptr << endl;
 }
+
+// constants won't change
+const int buttonPin = 2;
+const int ledPin = 13;
+
+// variables will change
+int buttonState = 0;
+
+void setup() {
+    // init the LED pin as an output
+    pinMode(ledPin, OUTPUT);
+    // init the pushbutton pin as an input
+    pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+    // read the state of the pushbutton value
+    buttonState = digitalRead(buttonPin);
+
+    // check if the pushbutton is pressed
+    if (buttonState == HIGH) {
+        // LED on
+        digitalWrite(ledPin, HIGH);
+    } else {
+        // LED off
+        digitalWrite(ledPin, low);
+    }
+}
