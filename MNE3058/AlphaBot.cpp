26a6cd752fc21b9158031473722dd77d227c3d04
meatsby <meatsby@gmail.com>
2023-05-07 21:02:47 +0800

    feat: line tracking

--- MNE3058/AlphaBot.cpp
+++ MNE3058/AlphaBot.cpp
@@ -1,264 +1,276 @@
-#include "Adafruit_NeoPixel.h"
-#include "Adafruit_GFX.h"
-#include "Adafruit_SSD1306.h"
 #include "TRSensors.h"
 #include <Wire.h>
 
-#define PWMA   6           //Left Motor Speed pin (ENA)
-#define AIN2   A0          //Motor-L forward (IN2).
-#define AIN1   A1          //Motor-L backward (IN1)
-#define PWMB   5           //Right Motor Speed pin (ENB)
-#define BIN1   A2          //Motor-R forward (IN3)
-#define BIN2   A3          //Motor-R backward (IN4)
+#define PWMA   6            //Left motor speed control with PWM signal.
+#define AIN2   A0           //Left motor forward.
+#define AIN1   A1           //Left motor backward.
+#define PWMB   5            //Right motor speed control with PWM signal.
+#define BIN1   A2           //Right motor forward, the same direction as the left motor backward.
+#define BIN2   A3           //Right motor backward, the same direction as the left motor forward.
 #define PIN 7
-#define NUM_SENSORS 5
-#define OLED_RESET 9
-#define OLED_SA0   8
-#define Addr 0x20
+#define NUM_SENSORS 5       //Five IR sensors under the Base of the Alphabot-2.
+#define Addr 0x20           //Communication address for the joystick.
 
-#define beep_on  PCF8574Write(0xDF & PCF8574Read())
-#define beep_off PCF8574Write(0x20 | PCF8574Read())
 
-Adafruit_SSD1306 display(OLED_RESET, OLED_SA0);
-
+//Global variables to be stored regardless of the loop function
 TRSensors trs =   TRSensors();
 unsigned int sensorValues[NUM_SENSORS];
-unsigned int last_proportional = 0;
+unsigned int error_prior = 0;
 unsigned int position;
 long integral = 0;
-uint16_t i, j;
+uint16_t i, j, k, m;
 byte value;
-unsigned long lasttime = 0;
-Adafruit_NeoPixel RGB = Adafruit_NeoPixel(4, PIN, NEO_GRB + NEO_KHZ800);
+bool FOUND = true;                              //Boolean data type variable to ensure the robot is on the line.
 
-void PCF8574Write(byte data);
+void PCF8574Write(byte data);                   //Communication for the joystick.
 byte PCF8574Read();
-uint32_t Wheel(byte WheelPos);
 
 void setup() {
-    Serial.begin(115200);
-    // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
-    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
-    // init done
+    Serial.begin(115200);                         //Serial communication begins with 115200 bauds.
+    Wire.begin();                                 //Communication with the joystick.
 
-    // Show image buffer on the display hardware.
-    // Since the buffer is intialized with an Adafruit splashscreen
-    // internally, this will display the splashscreen.
-    display.clearDisplay();
-    display.setTextSize(2);
-    display.setTextColor(WHITE);
-    display.setCursor(10,0);
-    display.println("WaveShare");
-    display.setCursor(10,25);
-    display.println("AlphaBot2");
-    display.setTextSize(1);
-    display.setCursor(10,55);
-    display.println("Press to calibrate");
-    display.display();
-
-    while(value != 0xEF)  //wait button pressed
-    {
-        PCF8574Write(0x1F | PCF8574Read());
-        value = PCF8574Read() | 0xE0;
+    while (value != 0xEF) {                       //If the joystick button is not pressed,
+        PCF8574Write(0x1F | PCF8574Read());         //Waits until the press is detected.
+        value = PCF8574Read() | 0xE0;               //0xEF = 0b11101111, the button pressed.
     }
 
-    pinMode(PWMA,OUTPUT);
-    pinMode(AIN2,OUTPUT);
-    pinMode(AIN1,OUTPUT);
-    pinMode(PWMB,OUTPUT);
-    pinMode(AIN1,OUTPUT);
-    pinMode(AIN2,OUTPUT);
-    analogWrite(PWMA,0);
-    analogWrite(PWMB,0);
-    digitalWrite(AIN2,HIGH);
-    digitalWrite(AIN1,LOW);
-    digitalWrite(BIN1,HIGH);
-    digitalWrite(BIN2,LOW);
-    RGB.begin();
-    RGB.setPixelColor(0,0x00FF00 );
-    RGB.setPixelColor(1,0x00FF00 );
-    RGB.setPixelColor(2,0x00FF00 );
-    RGB.setPixelColor(3,0x00FF00);
-    RGB.show();
-    delay(500);
-    analogWrite(PWMA,80);
-    analogWrite(PWMB,80);
-    for (int i = 0; i < 100; i++)  // make the calibration take about 10 seconds
-    {
-        if(i<25 || i >= 75)
+    pinMode(PWMA, OUTPUT);
+    pinMode(AIN2, OUTPUT);
+    pinMode(AIN1, OUTPUT);
+    pinMode(PWMB, OUTPUT);
+    pinMode(AIN1, OUTPUT);
+    pinMode(AIN2, OUTPUT);
+    analogWrite(PWMA, 0);
+    analogWrite(PWMB, 0);
+    digitalWrite(AIN2, HIGH);
+    digitalWrite(AIN1, LOW);
+    digitalWrite(BIN1, HIGH);
+    digitalWrite(BIN2, LOW);
+    analogWrite(PWMA, 80);
+    analogWrite(PWMB, 80);
+
+    for (int i = 0; i < 100; i++) {               //Make the calibration take about 10 seconds
+        if (i < 25 || i >= 75)
         {
-            digitalWrite(AIN1,HIGH);
-            digitalWrite(AIN2,LOW);
-            digitalWrite(BIN1,LOW);
-            digitalWrite(BIN2,HIGH);
+            digitalWrite(AIN1, HIGH);
+            digitalWrite(AIN2, LOW);
+            digitalWrite(BIN1, LOW);
+            digitalWrite(BIN2, HIGH);
         }
-        else
-        {
-            digitalWrite(AIN1,LOW);
-            digitalWrite(AIN2,HIGH);
-            digitalWrite(BIN1,HIGH);
-            digitalWrite(BIN2,LOW);
+        else {
+            digitalWrite(AIN1, LOW);
+            digitalWrite(AIN2, HIGH);
+            digitalWrite(BIN1, HIGH);
+            digitalWrite(BIN2, LOW);
         }
-        trs.calibrate();       // reads all sensors 100 times
+        trs.calibrate();
     }
-    analogWrite(PWMA,0);
-    analogWrite(PWMB,0);
-    digitalWrite(AIN2,LOW);
-    digitalWrite(AIN1,LOW);
-    digitalWrite(BIN1,LOW);
-    digitalWrite(BIN2,LOW);
-    RGB.setPixelColor(0,0x0000FF );
-    RGB.setPixelColor(1,0x0000FF );
-    RGB.setPixelColor(2,0x0000FF );
-    RGB.setPixelColor(3,0x0000FF);
-    RGB.show(); // Initialize all pixels to 'off'
+    analogWrite(PWMA, 0);                         //After the calibration is done, stop and wait.
+    analogWrite(PWMB, 0);
+    digitalWrite(AIN2, LOW);
+    digitalWrite(AIN1, LOW);
+    digitalWrite(BIN1, LOW);
+    digitalWrite(BIN2, LOW);
 
     value = 0;
-    while(value != 0xEF)  //wait button pressed
-    {
+
+    //Wait until the joystick button is pressed.
+    while (value != 0xEF) {
         PCF8574Write(0x1F | PCF8574Read());
         value = PCF8574Read() | 0xE0;
-        position = trs.readLine(sensorValues)/200;
-        display.clearDisplay();
-        display.setCursor(0,25);
-        display.println("Calibration Done !!!");
-        display.setCursor(0,55);
-        for (int i = 0; i < 21; i++)
-        {
-            display.print('_');
-        }
-        display.setCursor(position*6,55);
-        display.print("**");
-        display.display();
+        position = trs.readLine(sensorValues) / 200;
+        delay(100);
+        digitalWrite(AIN1, LOW);
+        digitalWrite(AIN2, HIGH);
+        digitalWrite(BIN1, LOW);
+        digitalWrite(BIN2, HIGH);
     }
-
-    display.clearDisplay();
-    display.setTextSize(2);
-    display.setTextColor(WHITE);
-    display.setCursor(10,0);
-    display.println("AlphaBot2");
-    display.setTextSize(3);
-    display.setCursor(40,30);
-    display.println("Go!");
-    display.display();
-
-    delay(500);
-    digitalWrite(AIN1,LOW);
-    digitalWrite(AIN2,HIGH);
-    digitalWrite(BIN1,LOW);
-    digitalWrite(BIN2,HIGH);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    // Get the position of the line.  Note that we *must* provide
-    // the "sensors" argument to read_line() here, even though we
-    // are not interested in the individual sensor readings.
-    //trs.AnalogRead(sensorValues);
-    position = trs.readLine(sensorValues);
-    for (unsigned char i = 0; i < NUM_SENSORS; i++)
-    {
-        Serial.print(sensorValues[i]);
-        Serial.print('\t');
-    }
-    Serial.println();
-    Serial.println(position); // comment this line out if you are using raw values
+    trs.readLine(sensorValues);                 //Read the sensor values for every loop.
 
-    // The "proportional" term should be 0 when we are on the line.
-    int proportional = (int)position - 2000;
-
-    // Compute the derivative (change) and integral (sum) of the position.
-    int derivative = proportional - last_proportional;
-    integral += proportional;
-
-    // Remember the last position.
-    last_proportional = proportional;
-
-    // Compute the difference between the two motor power settings,
-    // m1 - m2.  If this is a positive number the robot will turn
-    // to the right.  If it is a negative number, the robot will
-    // turn to the left, and the magnitude of the number determines
-    // the sharpness of the turn.
-    int power_difference = proportional/20 + integral/10000 + derivative*10;
-
-    // Compute the actual motor settings.  We never set either motor
-    // to a negative value.
-    const int maximum =100;//original number is maximum 255
-
-    if (power_difference > maximum)
-        power_difference = maximum;
-    if (power_difference < - maximum)
-        power_difference = - maximum;
-    Serial.println(power_difference);
-
-    if (power_difference < 0)
-    {
-        analogWrite(PWMA,maximum + power_difference);
-        analogWrite(PWMB,maximum);
-    }
-    else
-    {
-        analogWrite(PWMA,maximum);
-        analogWrite(PWMB,maximum - power_difference);
+    //All the sensors read bright
+    if (sensorValues[0] < 20 && sensorValues[1] < 20 && sensorValues[2] < 20 && sensorValues[3] < 20 && sensorValues[4] < 20) {
+        FOUND = false;
+        analogWrite(PWMA, 0);
+        analogWrite(PWMB, 0);
+        digitalWrite(AIN1, HIGH);
+        digitalWrite(AIN2, LOW);
+        digitalWrite(BIN1, HIGH);
+        digitalWrite(BIN2, LOW);
+        analogWrite(PWMA, 60);                    //Move backward slightly to check the last position on the line.
+        analogWrite(PWMB, 60);
+        delay(100);
+        position = trs.readLine(sensorValues);    //Read the last position.
     }
 
-    if (sensorValues[1] > 900 && sensorValues[2] > 900 && sensorValues[3] > 900)
-    {
-        // There is no line .Must Stop.
-        analogWrite(PWMA,0);
-        analogWrite(PWMB,0);
-    }
+    //If the last position was the same as the desired position, move forward with searching every 1 millisecond for the next line.
+    if (FOUND == false && position == 2000) {
+        digitalWrite(AIN1, LOW);
+        digitalWrite(AIN2, HIGH);
+        digitalWrite(BIN1, LOW);
+        digitalWrite(BIN2, HIGH);
+        analogWrite(PWMA, 180);
+        analogWrite(PWMA, 180);
+        //delay(50);
 
-//  PCF8574Write(0xC0 | PCF8574Read());   //set Pin High
-//  value = PCF8574Read() | 0x3F;         //read Pin
-//  while(value != 0xFF)
-//  {
-//    PCF8574Write(0xC0 | PCF8574Read());   //set Pin High
-//    value = PCF8574Read() | 0x3F;         //read Pin
-//    analogWrite(PWMA,0);
-//    analogWrite(PWMB,0);
-//    beep_on;
-//  }
-//  beep_off;
-
-    if(millis() - lasttime >200){
-        lasttime = millis();
-        for(i=0; i< RGB.numPixels(); i++) {
-            RGB.setPixelColor(i, Wheel(((i * 256 / RGB.numPixels()) + j) & 255));
+        //Path searching for 100 ms.
+        for (int i=0; i < 100; i++) {
+            delay(1);
+            trs.readLine(sensorValues);
+            if (sensorValues[0] > 50 || sensorValues[1] > 50 || sensorValues[2] > 50 || sensorValues[3] > 50 || sensorValues[4] > 50) {
+                FOUND = true;
+                error_prior = 0;                        //If any of five sensors reads a color, get ready to restart the PID control by initializing the integral and prior error value.
+                integral = 0;
+                break;
+            }
         }
-        RGB.show();
-        if(j++ > 256*4) j= 0;
     }
 
-    //delay(250);
-}
+    //If the robot was supposed to go to the right before derailing, turn right slightly and move forward to search.
+    if (FOUND == false && position > 2000) {
+        digitalWrite(AIN1, LOW);
+        digitalWrite(AIN2, HIGH);
 
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-    if(WheelPos < 85) {
-        return RGB.Color(WheelPos * 50, 255 - WheelPos * 50, 0);
-    } else if(WheelPos < 170) {
-        WheelPos -= 85;
-        return RGB.Color(255 - WheelPos * 50, 0, WheelPos * 50);
-    } else {
-        WheelPos -= 170;
-        return RGB.Color(0, WheelPos * 50, 255 - WheelPos * 50);
+        analogWrite(PWMA, 90);
+        analogWrite(PWMB, 90);
+        delay(100);
+
+        digitalWrite(BIN1, LOW);
+        digitalWrite(BIN2, HIGH);
+
+        analogWrite(PWMA, 100);
+        analogWrite(PWMB, 100);
+        //delay(50);
+
+        //Searching right
+        for (int j = 0; j < 40; j++) {
+            delay(1);
+            trs.readLine(sensorValues);
+            if (sensorValues[0] > 50 || sensorValues[1] > 50 || sensorValues[2] > 50 || sensorValues[3] > 50 || sensorValues[4] > 50) {
+                FOUND = true;
+                error_prior = 0;                          //Initialize the PID function. Otherwise, it will rotate at
+                integral = 0;                             //one spot as integral is a hugely large number.
+                Break;
+            }
+        }
+
+        if (FOUND == false) {                         //If the robot cannot find the line by forward searching,
+            analogWrite(PWMA, 10);                      //begin to search by move circularly because it can find the line as the rail was broken
+            analogWrite(PWMB, 130);                     //somewhere at the right of the last position.
+            for (int k = 0; k < 300; k++) {
+                delay(1);
+                trs.readLine(sensorValues);
+                if (sensorValues[0] > 50 || sensorValues[1] > 50 || sensorValues[2] > 50 || sensorValues[3] > 50 || sensorValues[4] > 50) {
+                    analogWrite(PWMA, 0);
+                    analogWrite(PWMB, 0);
+                    FOUND = true;
+                    error_prior = 0;
+                    integral = 0;
+                    break;
+                }
+            }
+        }
+    }
+
+    //If the robot was supposed to go to the left, turn left and do the forward searching for a moment.
+    if (FOUND == false && position < 2000) {
+        digitalWrite(BIN1, LOW);
+        digitalWrite(BIN2, HIGH);
+
+        analogWrite(PWMA, 90);
+        analogWrite(PWMB, 90);
+        delay(100);
+
+        digitalWrite(AIN1, LOW);
+        digitalWrite(AIN2, HIGH);
+
+        analogWrite(PWMA, 100);
+        analogWrite(PWMB, 100);
+        //delay(50);
+
+        //Searching left
+        for (int m = 0; m < 40; m++) {
+            delay(1);
+            trs.readLine(sensorValues);
+            if (sensorValues[0] > 50 || sensorValues[1] > 50 || sensorValues[2] > 50 || sensorValues[3] > 50 || sensorValues[4] > 50) {
+                FOUND = true;
+                error_prior = 0;
+                integral = 0;
+                break;
+            }
+        }
+
+        //if the path not found, circular search
+        if (FOUND == false) {
+            analogWrite(PWMA, 130);
+            analogWrite(PWMB, 10);
+            for (int n = 0; n < 300; n++) {
+                delay(1);
+                trs.readLine(sensorValues);
+                if (sensorValues[0] > 50 || sensorValues[1] > 50 || sensorValues[2] > 50 || sensorValues[3] > 50 || sensorValues[4] > 50) {
+                    analogWrite(PWMA, 0);             //Initialize the PID function.
+                    analogWrite(PWMB, 0);
+                    FOUND = true;
+                    error_prior = 0;
+                    integral = 0;
+                    break;
+                }
+            }
+        }
+    }
+
+    position = trs.readLine(sensorValues);            //Decide position to run when the robot finds the line.
+
+    if (FOUND == true) {
+        int error, derivative, m_control;
+
+        error = (int)position - 2000;                   //Error feedback.
+        integral += error;
+        derivative = error - error_prior;
+        error_prior = error;
+
+        //PID: Kp, Ki, and Kd values obtained by some trials of tuning.
+        m_control = 0.075 * error + 0.0001 * integral + 20 * derivative;
+        const int maxspeed = 180;
+
+        if (m_control > maxspeed)                                     //PID control function may result in hugely high or low value, so we have to convert it to the maximum speed value.
+            m_control = maxspeed;
+        if (m_control < - maxspeed)
+            m_control = - maxspeed;
+
+        /*
+         * While following the path, there may be some distracts, such as a horizontal path across the original path.
+         * The original line is 2cm wide, so only the one sensor at the center fully reads the high value.
+         */
+        if (sensorValues[1] > 900 && sensorValues[2] > 900 && sensorValues[3] > 900) {
+            analogWrite(PWMA, maxspeed);
+            analogWrite(PWMB, maxspeed);
+        }
+        if (m_control > 0) {
+            analogWrite(PWMA, maxspeed);
+            analogWrite(PWMB, maxspeed - m_control);
+        }
+        else {
+            analogWrite(PWMA, maxspeed + m_control);
+            analogWrite(PWMB, maxspeed);
+        }
     }
 }
 
-void PCF8574Write(byte data)
-{
+//PCF8574 is a chip to transmit and convert the input value of the joystick to the digital value.
+void PCF8574Write(byte data) {
     Wire.beginTransmission(Addr);
     Wire.write(data);
     Wire.endTransmission();
 }
 
-byte PCF8574Read()
-{
+
+byte PCF8574Read() {
     int data = -1;
     Wire.requestFrom(Addr, 1);
-    if(Wire.available()) {
+    if (Wire.available()) {
         data = Wire.read();
     }
     return data;
